fecc3b8c54d4449f8e7196817027507d9e04a92a
Alade1 <aladefolarin7@gmail.com>
2024-01-02 22:44:48 +0100

    Mobile portfolio responsive

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const DropDownButton  = document.querySelector(".menu")
+    const DropDownMenu = document.querySelector(".menu-content")
+
+    DropDownButton.addEventListener("click", function(){
+        DropDownMenu.style.display = DropDownMenu.style.display === "block" ? "none" : "block";
+    
+     })
+
+
+
+})
